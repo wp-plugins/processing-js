0ed67da23e54b330695110ad078bd98dd1140b15
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-30 11:20:29 +0000

    v1.3.6 in trunk
    git-svn-id: https://plugins.svn.wordpress.org/processing-js/trunk@457336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- processing-js.php
+++ processing-js.php
@@ -3,7 +3,7 @@
 Plugin Name: Processing.js
 Plugin URI: http://www.ramoonus.nl/wordpress/processing-js/
 Description: Processing.js is the sister project of the popular Processing visual programming language, designed for the web. Processing.js makes your data visualizations, digital art, interactive animations, educational graphs, video games, etc. work using web standards and without any plug-ins. 
-Version: 1.3.5
+Version: 1.3.6
 Author: Ramoonus
 Author URI: http://www.ramoonus.nl/
 License: GPL2
@@ -11,7 +11,7 @@ License: GPL2
 /* Javascript */
 function rw_processing() {
 	wp_deregister_script('processing'); // deregister
-	wp_enqueue_script('processing', plugins_url('/js/processing-1.3.5.min.js', __FILE__), false, '1.3.5');
+	wp_enqueue_script('processing', plugins_url('/js/processing-1.3.6.min.js', __FILE__), false, '1.3.6');
 }
 add_action('init', 'rw_processing');
 
